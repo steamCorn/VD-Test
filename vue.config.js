df9fce734750d3f4a70afa4a1391874d1cf7722d
Marina Parinova <parinova@d-tt.nl>
2023-09-19 11:47:07 +0200

    Found that datepicker library has a problem with the loader that failed the build. I added datepicker @vuepic/vue-datepicker 6.1.0 tested out issue. Going to create my date input field

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 // module.exports = {
 //   lintOnSave: true,
+//   parallel: false,
 //   configureWebpack: {
 //     module: {
 //       rules: [
 //         {
-//           test: /\.jsx?$/,
-//           loader: 'babel-loader',
-//           exclude: /node_modules\/(?!(v-calendar)\/).*/,
+//           // test: /\.jsx?$/,
+//           // loader: 'babel-loader',
+//           // exclude: /node_modules\/(?!(v-calendar)\/).*/,
 //         },
 //       ],
 //     },
